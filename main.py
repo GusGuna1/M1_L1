522c97edfbc1adbd416b3e23bacf527ce9edb805
GusGuna1 <166997061+GusGuna1@users.noreply.github.com>
2024-05-05 20:35:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-meme_dict = {
-            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
-            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
-            }
-word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
-if word in meme_dict.keys():
-    # Apa yang harus kita lakukan jika kata itu ditemukan?
-    print(meme_dict[word])
-    
-else:
-    # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
-    print( "not in dictionry")
+import random
+def gen_pass(pass_length):
+   char = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+   p = ""
+   for i in range(pass_length):
+      p = p+random.choice(char)
+   return p
